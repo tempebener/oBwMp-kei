e157f4385e8a5c676fc4e4fc845b49615cd2e3b0
tempebener <bayoewa@gmail.com>
2021-03-27 16:47:57 +0700

    background pendaftaran jd merah

--- application/views/frontend/pendaftaran.php
+++ application/views/frontend/pendaftaran.php
@@ -1,7 +1,7 @@
- <section class="section-sm bg-primary context-dark text-center">
+ 	<section class="section-sm bg-red context-dark text-center">
         <div class="container">
           <div class="group-16 d-lg-flex flex-wrap justify-content-between align-items-center">
-            <h3>Prepare Your Application Letter for Admission</h3><a class="btn btn-primary" href="#">Apply</a>
+            <h3>Prepare Your Application Letter for Admission</h3><a class="btn btn-red" href="#">Apply</a>
           </div>
         </div>
-      </section>
+    </section>

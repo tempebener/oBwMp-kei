69a743c465a1906e0d92cdf55ab668a602334b32
tempebener <bayoewa@gmail.com>
2021-03-27 11:40:40 +0700

    as

--- application/views/depan/modul_pelatihan.php
+++ application/views/depan/modul_pelatihan.php
@@ -1,4 +1,6 @@
-  <section class="section-md bg-300 text-center">
+
+      <!-- Price box-->
+      <section class="section-md bg-300 text-center">
         <div class="container">
           <div class="text-block text-block-1" data-animate='{"class":"fadeIn"}'>
             <h2>Our Featured Courses</h2>
@@ -6,21 +8,55 @@
           </div>
           <div class="owl-carousel owl-content-1" data-owl="{&quot;dots&quot;:true}" data-loop="false" data-items="1" data-sm-items="2" data-md-items="3" data-lg-items="4">
                   <!-- Price box-->
-                  <?php
-              foreach ($modul_pelatihan as $post_new)
-              {
-              
-              ?>
                   <div class="price-box">
-                    <div class="price-box-media"><img class="price-box-img" src="<?php echo base_url()?>asset/foto_pelatihan/<?php echo $post_new->foto_pelatihan ?>" alt="" width="270" height="220"/>
-                    <!--   <div class="price-box-tag"></div> -->
+                    <div class="price-box-media"><img class="price-box-img" src="images/image-01-270x220.jpg" alt="" width="270" height="220"/>
+                      <div class="price-box-tag">$20</div>
                     </div>
                     <div class="price-box-body">
-                      <div class="price-box-title h6"><a href="#"><?php echo $post_new->judul_pelatihan ?></a></div>
-                     <!--  <div class="price-box-text"></div> -->
+                      <div class="price-box-title h6"><a href="#">Academic English</a></div>
+                      <div class="price-box-text">Steven Carter</div>
                     </div>
                   </div>
-                   <?php } ?>
+                  <!-- Price box-->
+                  <div class="price-box">
+                    <div class="price-box-media"><img class="price-box-img" src="images/image-02-270x220.jpg" alt="" width="270" height="220"/>
+                      <div class="price-box-tag">$40</div>
+                    </div>
+                    <div class="price-box-body">
+                      <div class="price-box-title h6"><a href="#">Online Marketing</a></div>
+                      <div class="price-box-text">Ronald Richards</div>
+                    </div>
+                  </div>
+                  <!-- Price box-->
+                  <div class="price-box">
+                    <div class="price-box-media"><img class="price-box-img" src="images/image-03-270x220.jpg" alt="" width="270" height="220"/>
+                      <div class="price-box-tag">$30</div>
+                    </div>
+                    <div class="price-box-body">
+                      <div class="price-box-title h6"><a href="#">HR Management</a></div>
+                      <div class="price-box-text">Ralph Edwards</div>
+                    </div>
+                  </div>
+                  <!-- Price box-->
+                  <div class="price-box">
+                    <div class="price-box-media"><img class="price-box-img" src="images/image-04-270x220.jpg" alt="" width="270" height="220"/>
+                      <div class="price-box-tag">$30</div>
+                    </div>
+                    <div class="price-box-body">
+                      <div class="price-box-title h6"><a href="#">Financial Analytics</a></div>
+                      <div class="price-box-text">Emma Smith</div>
+                    </div>
+                  </div>
+                  <!-- Price box-->
+                  <div class="price-box">
+                    <div class="price-box-media"><img class="price-box-img" src="images/image-18-270x220.jpg" alt="" width="270" height="220"/>
+                      <div class="price-box-tag">$30</div>
+                    </div>
+                    <div class="price-box-body">
+                      <div class="price-box-title h6"><a href="#">Web Technology</a></div>
+                      <div class="price-box-text">Kathryn Murphy</div>
+                    </div>
+                  </div>
           </div>
         </div>
       </section>

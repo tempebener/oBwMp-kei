02078013eb4a545ad898d2bb35259c5193ecf5ea
sansD92 <adjiedwisandy@gmail.com>
2021-03-27 16:07:38 +0700

    upuup

---
+++ application/views/frontend/footer.php
@@ -0,0 +1,63 @@
+ <footer class="section footer">
+        <div class="footer-top bg-300">
+          <div class="container">
+            <div class="row row-30">
+              <div class="col-xs-6 col-md-3">
+                <h5 class="footer-title">Contacts</h5>
+                <ul class="list list-icons">
+                  <li class="list-item">
+                    <div class="list-icon custom-font-pin"></div><a class="link link-inherit" href="#">Brooklyn, NY 10036, 16-2 United States</a>
+                  </li>
+                  <li class="list-item">
+                    <div class="list-icon custom-font-email"></div><a class="link link-inherit" href="mailto:#">info@demolink.org</a>
+                  </li>
+                  <li class="list-item">
+                    <div class="list-icon custom-font-phone"></div><span><span>Call Us: </span><a class="link link-inherit" href="tel:#">1-800-123-1234</a></span>
+                  </li>
+                </ul>
+              </div>
+              <div class="col-6 col-md-3">
+                <h5 class="footer-title">Socials</h5>
+                <ul class="list list-sm">
+                  <li class="list-item"><a class="link link-inherit" href="#">Facebook</a></li>
+                  <li class="list-item"><a class="link link-inherit" href="#">Pinterest</a></li>
+                  <li class="list-item"><a class="link link-inherit" href="#">LinkedIn</a></li>
+                  <li class="list-item"><a class="link link-inherit" href="#">Instagram</a></li>
+                  <li class="list-item"><a class="link link-inherit" href="#">Twitter</a></li>
+                  <li class="list-item"><a class="link link-inherit" href="#">YouTube</a></li>
+                </ul>
+              </div>
+              <div class="col-6 col-md-3">
+                <h5 class="footer-title">About Us</h5>
+                <ul class="list list-sm">
+                  <li class="list-item"><a class="link link-inherit" href="#">Our Mission</a></li>
+                  <li class="list-item"><a class="link link-inherit" href="#">Graduate Admission</a></li>
+                  <li class="list-item"><a class="link link-inherit" href="#">Our Vision</a></li>
+                  <li class="list-item"><a class="link link-inherit" href="#">Our Values</a></li>
+                  <li class="list-item"><a class="link link-inherit" href="#">About Us</a></li>
+                  <li class="list-item"><a class="link link-inherit" href="academics.html">Academics</a></li>
+                  <li class="list-item"><a class="link link-inherit" href="#">Degree Programs</a></li>
+                </ul>
+              </div>
+              <div class="col-6 col-md-3">
+                <h5 class="footer-title">Teachers</h5>
+                <ul class="list list-sm">
+                  <li class="list-item"><a class="link link-inherit" href="team-member.html">Olivia Thomas</a></li>
+                  <li class="list-item"><a class="link link-inherit" href="team-member.html">Alice Bohm</a></li>
+                  <li class="list-item"><a class="link link-inherit" href="team-member.html">Max Turner</a></li>
+                  <li class="list-item"><a class="link link-inherit" href="team-member.html">Jane Smith</a></li>
+                  <li class="list-item"><a class="link link-inherit" href="team-member.html">John Stevens</a></li>
+                  <li class="list-item"><a class="link link-inherit" href="team-member.html">Martha Stevens</a></li>
+                  <li class="list-item"><a class="link link-inherit" href="team-member.html">Connor Walker</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="footer-bottom bg-700 context-dark text-center">
+          <div class="container">
+                  <!-- Copyright-->
+                  <p class="rights"><span>&copy; 2021&nbsp;</span><span>K E I</span><span> All rights reserved.&nbsp;</span><a class="link link-inherit" href="privacy-policy.html"></a></p>
+          </div>
+        </div>
+      </footer>
---
+++ application/views/frontend/pendaftaran.php
@@ -0,0 +1,7 @@
+ <section class="section-sm bg-primary context-dark text-center">
+        <div class="container">
+          <div class="group-16 d-lg-flex flex-wrap justify-content-between align-items-center">
+            <h3>Prepare Your Application Letter for Admission</h3><a class="btn btn-primary" href="#">Apply</a>
+          </div>
+        </div>
+      </section>

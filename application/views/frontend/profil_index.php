da7d5b5ca8bbf11929fbb45bb00cd263308edec5
tempebener <bayoewa@gmail.com>
2021-03-30 17:31:50 +0700

    Profil update

--- application/views/frontend/profil_index.php
+++ application/views/frontend/profil_index.php
@@ -31,16 +31,27 @@
         <div class="container">
           <div class="text-block text-block-1">
             <h2>PROFIL JOGLO TUMIYONO</h2>
-            <p class="big">Massa, et porttitor cras sed ultrices ullamcorper mauris semper iaculis. Ornare suscipit purus nulla pharetra velit, leo in. Amet nulla nunc vitae viverra pellentesque in eget. Mi ut sit ultrices sed.</p>
+            <p class="big" style="text-align: left !important;">
+              Lokasi Joglo 
+              https://maps.app.goo.gl/PvJNhJXnneQc1VbG9 
+              <br/>
+Joglo Tumiyono adalah sebuah mahakarya yang dipersembahkan PT Widodo Makmur Perkasa sebagai bagian dari proses pelestarian  budaya Adiluhung Jawa khususnya terkait dengan seni bangunan dan pahat. Didedikasikan bagi generasi muda agar memiliki wawasan budaya dan menumbuhkan rasa memiliki terhadap budaya bangsa. Kerajaan dan Kesultanan di wilayah Jawa berdasarkan catatan sejarah adalah kerajaan besar yang memiliki wilayah yang luas dan berjaya diantara kerajaan lainnya di sekitar Asia. Tetapi beberapa keraton besar tersebut musnah tidak diketahui keberadaannya, tidak meninggalkan artefak dan catatan sejarah. Keraton Demak, Keraton Majapahit, Keraton Singasari, Kesultanan Demak, Kesultanan Pajang semua hilang tanpa bekas sehingga kita tidak mengetahui kebesaran pada masa lalu.
+              <br/>
+Beruntungnya Kesultanan Yogyakarta dan Kasunanan Surakarta masih terjaga keberadaannya sehingga bisa kita nikmati saat ini dan menjadi referensi pembangunan Joglo Tumiyono. Joglo Tumiyono didirikan bukan hanya sebagai monumen dan penambah wawasan, tetapi juga memiliki visi yang mulia  sebagai pusat kegiatan masyarakat. Joglo Tumiyono didirikan atas prakarsa pendiri PT Widodo Makmur Perkasa dan didukung oleh seluruh keluarga besar Tri Widodo untuk mendirikan sebuah bangunan joglo termegah di dunia sebagai monumen dan pelestarian budaya Jawa yang sarat nilai-nilai kehidupan. Joglo Tumiyono didedikasikan bagi pengembangan masyarakat, tempat pendidikan dan pelatihan generasi muda sebagai bentuk pengabdian pada bangsa. Menempati luas lahan 2,2 hektar di Desa Ngerangan, Kecamatan Bayat, Kabupaten Klaten, Provinsi Jawa Tengah. Berada diantara kawasan pegunungan, Gunung Merapi, Makam Sunan Bayat, lokasi petilasan jalur gerilya Pangeran Diponegoro dan berdekatan dengan kawasan wisata Umbul Ponggok.
+              <br/>
+Peletakan batu pertama pada bulan Oktober 2017 dengan ditancapkannya paku emas di puncak bangunan serta keempat soko guru. Joglo Tumiyono dibangun berdasarkan konsep kawasan Joglo terpadu yang dibuat oleh para arsitek muda cerdas di JOSO, konsultan arsitek dan merupakan anak perusahaan PT Widodo Makmur Perkasa yang berkolaborasi dengan pihak-pihak terkait lainnya. Joglo Tumiyono dibangun dari sebuah mimpi menghadirkan sebuah mahakarya seni yang bermanfaat bagi anak negeri. Sebagian besar material bangunan Joglo Tumiyono merupakan kayu jati berkualitas tinggi dengan nilai material tak terhingga. Melibatkan lebih dari 60 seniman ukir yang didatangkan khusus dari Jepara yang dikenal sebagai Kota Ukir dan pencipta salah satu Langgam Ukir Nusantara. Pembangunan Joglo Tumiyono juga melibatkan para arsitek, ahli Teknik sipil, para perajin seniman lampu, patung, arca, batu bata, para tukang kayu dan pekerja bangunan. Dilibatkannya para seniman batu gunung Merapi dari Muntilan, lampu chandelier perunggu buatan dari seniman Jogjakarta,  begitu juga seniman patung tembaga terbaik dari Yogyakarta, perajin genting tradisional dan batu bata Bali. Semua yang ditunjuk merupakan seniman dan produsen terbaik di bidangnya.
+              <br/>
+Semua bekerjasama sesuai keahliannya dengan penuh dedikasi memberikan kontribusi terbaik demi terwujudnya Joglo Tumiyono yang megah dan bisa kita nikmati dengan rasa kagum saat ini. Melibatkan alat-alat berat dalam memasang tiang pancang dan membangun pondasi Joglo yang kuat agar stabil dan tahan gempa merupakan komitmen bahwa Joglo Tumiyono akan mampu bertahan melewati perubahan jaman selama dalam pemeliharaan yang berkelanjutan. Joglo Tumiyono akan menjadi pusat pelatihan dan pembelajaran para entrepreneur muda yang terdidik dan terampil agar siap menjadi pelaku ekonomi berdasarkan minat mereka di bidang peternakan, pertanian atau pengolahan bahan makanan. Para entrepreneur muda ini akan dididik agar mampu mengelola bisnisnya dengan baik serta sustainability, yang merupakan sebuah kondisi dimana usaha dapat terus bertahan dan berkembang seiring waktu.
+</p>
           </div>
           <div class="row no-gutters">
-            <div class="col-6"><img src="<?php echo base_url();?>theme/frontend/images/image-10-390x390.jpg" alt="" width="390" height="390"/>
+            <div class="col-6"><img src="<?php echo base_url();?>theme/images/profil/1.jpg" alt="" width="390" height="390"/>
             </div>
-            <div class="col-6"><img src="<?php echo base_url();?>theme/frontend/images/image-11-390x390.jpg" alt="" width="390" height="390"/>
+            <div class="col-6"><img src="<?php echo base_url();?>theme/images/profil/2.jpg" alt="" width="390" height="390"/>
             </div>
-            <div class="col-6"><img src="<?php echo base_url();?>theme/frontend/images/image-12-390x390.jpg" alt="" width="390" height="390"/>
+            <div class="col-6"><img src="<?php echo base_url();?>theme/images/profil/3.jpg" alt="" width="390" height="390"/>
             </div>
-            <div class="col-6"><img src="<?php echo base_url();?>theme/frontend/images/image-12-390x390.jpg" alt="" width="390" height="390"/>
+            <div class="col-6"><img src="<?php echo base_url();?>theme/images/profil/4.jpg" alt="" width="390" height="390"/>
             </div>
           </div>
         </div>

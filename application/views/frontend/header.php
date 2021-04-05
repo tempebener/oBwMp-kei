2dda812e91272d87c4241372e2b0e73d284a3b5f
tempebener <bayoewa@gmail.com>
2021-04-05 16:55:17 +0700

    update menu

--- application/views/frontend/header.php
+++ application/views/frontend/header.php
@@ -14,7 +14,7 @@
                 <li class="navbar-navigation-root-item"><a class="navbar-navigation-root-link" href="#">Tentang Kami</a>
                   <ul class="navbar-navigation-dropdown rd-navbar-dropdown">
 
-                    <li class="navbar-navigation-dropdown-item"><a class="navbar-navigation-dropdown-link" href="#">Kesatriaan Entrepreneur Indonesia</a>
+                    <li class="navbar-navigation-dropdown-item"><a class="navbar-navigation-dropdown-link" href="#">KEI</a>
                       <ul class="navbar-navigation-dropdown rd-navbar-dropdown">
                         <li class="navbar-navigation-dropdown-item">
                           <a class="navbar-navigation-dropdown-link" href="<?php echo base_url();?>visi_misi">Visi & Misi</a>

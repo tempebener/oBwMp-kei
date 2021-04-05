ad676ae7827450f4d93c05d7b845a439ae949888
sansD92 <adjiedwisandy@gmail.com>
2021-04-05 18:20:32 +0700

    up

--- application/controllers/Pelatihan.php
+++ application/controllers/Pelatihan.php
@@ -8,13 +8,14 @@ class Pelatihan extends CI_Controller {
     parent::__construct();
     /* memanggil model untuk ditampilkan pada masing2 modul */
 	$this->load->model('Model_utama');
+	$this->load->model('M_pelatihan');
     /* memanggil function dari masing2 model yang akan digunakan */
 
   }
 
 	
 
-public function detail_pelatihan(){
+public function detail_pelatihan($id){
 
 			
 			$query = $this->Model_utama->view_where('tbl_pelatihan',array('judul_pelatihan_seo' => $this->uri->segment(3)),'id_pelatihan','DESC',0,1);
@@ -25,7 +26,10 @@ public function detail_pelatihan(){
 				$row = $query->row_array();
 				$data['title'] = cetak($row['judul_pelatihan']);
 				$data['description'] = cetak_meta($row['deskripsi_singkat'],0,400);
-				$data['rows'] = $row;		 
+				$data['rows'] = $row;		
+
+				 $data['pelatihan_detail']            = $this->M_pelatihan->get_by_id_pelatihan($id);
+ 
 				  
 
 				$this->load->view('frontend/modul_pelatihan_singel', $data);

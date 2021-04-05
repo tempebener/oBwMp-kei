aec65844611dfeab611a6170f73fc62429ff017a
tempebener <bayoewa@gmail.com>
2021-04-05 18:32:11 +0700

    meat

--- application/views/frontend/tk_meat.php
+++ application/views/frontend/tk_meat.php
@@ -109,6 +109,76 @@
            
         </div>
       </section>
+
+      <br/>
+      <!-- Deskripsi PWM -->
+      <section class="section-md bg-transparent text-center">
+        <div class="container">
+          <div class="text-block text-block-1">
+            <h2>PT PRIMA WIDODO MAKMUR</h2>
+            <p class="big" style="text-align: justify !important;">
+              &emsp;&emsp;PT Prima Widodo Makmur (PWM) merupakan anak perusahaan PT Cianjur Arta Makmur  yang bergerak di bidang produksi makanan olahan bersumber dari produk sapi dan unggas. Beberapa produk unggulan yang diproduksi PWM adalah sosis, bakso, naget, patty sapi dan ayam, pao dalam berbagai varian rasa, ukuran dan harga. Keunggulan sosis PWM pada kualitas bahan baku utama yang terjaga baik dari segi kualitas maupun kuantitasnya karena berasal dari RPH internal dan sapi-sapi berkualitas dan sehat dari peternakan terintegrasi PT Pasir Tengah. Begitupun untuk produk bakso akan dijaga kualitas rasa dan bahan bakunya yang juga bersumber dari RPH internal.
+              <br/><br/>
+              &emsp;&emsp;Naget, patty ayam, bakso ayam dan spicy wing diproduksi dengan memanfaatkan bahan baku unggas yang disuplai dari anak perusahaan WMP lainnya, yaitu PT Widodo Makmur Unggas yang memproduksi ayam broiler berkualitas prima karena diternakkan melalui modern closed house farm yang menjaga kualitas hidup ayam terprogram pemberian makannya, jenis-jenis bahan pakannya melalui seleksi ketat dan kandang terjaga higienitas dan kenyamanannya.
+              <br/><br/>
+              &emsp;&emsp;Semua produk olahan ini dijalankan menggunakan mesin modern berasal dari Eropa untuk memastikan kualitas produk sesuai yang diharapkan. Penerapan SOP produksi pangan yang ketat dijalankan, agar produk makanan yang dihasilkan terjaga higienitasnya. PWM telah memiliki pangsa pasar yang luas meliputi daerah JABODETABEK, seluruh Pulau Jawa  dan Bali, serta sebagian wilayah Sumatra dan Sulawesi. PWM juga mencanangkan ekspor ke berbagai wilayah Asia, sebagian wilayah Timur Tengah dll.
+              </p>
+          </div>
+
+          <div class="row">
+            <div class="col-md-6">
+                <img class="image-custom rounded" src="<?php echo base_url();?>theme/images/unit/meat/pwm1.png" alt="pwm1" width="100%" height="auto" style="float:left;">
+              <!-- <a class="btn btn-outline" href="#">Learn more</a> -->
+            </div>
+            <div class="col-md-6">
+                <img class="image-custom rounded" src="<?php echo base_url();?>theme/images/unit/meat/pwm2.png" alt="pwm2" width="100%" height="auto" style="float:left;">
+              <!-- <a class="btn btn-outline" href="#">Learn more</a> -->
+            </div>
+        </div>
+      </section>
+
+      <br/>
+      <!-- Deskripsi GMP -->
+      <section class="section-md bg-transparent text-center">
+        <div class="container">
+          <div class="text-block text-block-1">
+            <img class="image-custom rounded" src="<?php echo base_url();?>theme/images/unit/meat/gmp1.png" alt="pwm1" width="100%" height="auto" style="float:left;margin-bottom: 30px;">
+            <h2>PT GARUT MAKMUR PERKASA</h2>
+            <p class="big" style="text-align: justify !important;">
+              <img src="<?php echo base_url();?>theme/images/unit/meat/gmp2.png" alt="PT. Garut Makmur Perkasa" width="250px" height="auto" style="float:left;">
+              &emsp;&emsp;PT Garut Makmur Perkasa (GMP) adalah perusahaan yang bergerak di bidang penyamakan kulit. Berlokasi di Garut, Jawa Barat yang telah dikenal sebagai pusat penghasil produk kulit berkualitas selama puluhan tahun.
+              <br/><br/>
+              &emsp;&emsp;Widodo Makmur Perkasa melihat bahwa industri penyamakan kulit memiliki potensi besar untuk dikembangkan, mengingat kebutuhan bahan baku baku kulit untuk industri fashion dan accessories terus meningkat setiap tahunnya.
+              <br/><br/>
+              &emsp;&emsp;Sebagai perusahan yang bergerak di bidang peternakan sapi terintegrasi, maka produk sampingan yang dihasilkan dari produksi daging berupa kulit sapi diolah menjadi blue leather dan kulit yang siap diolah lainya. pemilihan  industri penyamakan kulit dalam lingkaran bisnisnya merupakan langkah tepat untuk menyerap hasil sampingan Rumah Potong Hewan.
+              <br/><br/>
+              &emsp;&emsp;Fashion dan segala pernak-pernik accessories telah menjadi kebutuhan manusia modern. Fungsi pakaian telah bergeser dari sekedar pelindung tubuh menjadi penunjuk identitas seseorang dalam lingkungan sosial dan ekonomi. Baju dan sepatu tertentu merujuk pada satu identitas pemakainya. Begitupun mobil mewah berkelas premium seperti Ferrari, Mercedez Benz, BMW, AUDI, atau Porsche menggunakan material kulit sapi sebagai car seat, pelapis dashboard dan interior di dalamnya.  Motor Besar juga mengunakan material kulit material jok dan  pelapis setang. Juga accessories jaket kulit dan sepatu kulit  adalah pelengkap yang tak pernah ditinggalkan para pecinta Moge.
+              <br/><br/>
+              &emsp;&emsp;Begitupun rumah para crème de la crème dilengkapi dengan sofa berbahan kulit kelas premium untuk mempertegas status ekonomi dan sosial  penghuninya, hal tersebut menjadi potensi besar untuk dikembangkan GMP.
+              </p>
+          </div>
+
+          <div class="row">
+            <div class="col-md-1"></div>
+            <div class="col-md-2">
+                <img class="image-custom rounded" src="<?php echo base_url();?>theme/images/unit/meat/gmp_bawah1.png" alt="pwm1" width="100%" height="auto" style="float:left;">
+            </div>
+            <div class="col-md-2">
+                <img class="image-custom rounded" src="<?php echo base_url();?>theme/images/unit/meat/gmp_bawah2.png" alt="pwm2" width="100%" height="auto" style="float:left;">
+            </div>
+            <div class="col-md-2">
+                <img class="image-custom rounded" src="<?php echo base_url();?>theme/images/unit/meat/gmp_bawah3.png" alt="pwm2" width="100%" height="auto" style="float:left;">
+            </div>
+            <div class="col-md-2">
+                <img class="image-custom rounded" src="<?php echo base_url();?>theme/images/unit/meat/gmp_bawah4.png" alt="pwm2" width="100%" height="auto" style="float:left;">
+            </div>
+            <div class="col-md-2">
+                <img class="image-custom rounded" src="<?php echo base_url();?>theme/images/unit/meat/gmp_bawah5.png" alt="pwm2" width="100%" height="auto" style="float:left;">
+            </div>
+            <div class="col-md-1"></div>
+        </div>
+      </section>
+
       <!-- Footer start-->
       <?php $this->load->view('frontend/footer')?>
       <!-- Footer end-->

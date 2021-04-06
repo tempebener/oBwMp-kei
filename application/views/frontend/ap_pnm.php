f7e92623fba39d791250b8522bf3a6f255b8cc66
tempebener <bayoewa@gmail.com>
2021-04-06 16:32:04 +0700

    Akses Permodalan

--- application/views/frontend/ap_pnm.php
+++ application/views/frontend/ap_pnm.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <!-- head -->
   <head>
-    <title>KUR</title>
+    <title>PNM</title>
     <?php $this->load->view('frontend/head2')?>
   </head>
 <!-- head end -->
@@ -24,8 +24,8 @@
             <div class="breadcrumb-inner">
               <div class="breadcrumb-item"><a class="breadcrumb-link" href="<?php echo base_url();?>">Home</a></div>
               <div class="breadcrumb-item"><span class="breadcrumb-text">Akses Permodalan</span></div>
-              <div class="breadcrumb-item"><span class="breadcrumb-text">Perbankan</span></div>
-              <div class="breadcrumb-item"><span class="breadcrumb-text breadcrumb-active">KUR</span></div>
+              <div class="breadcrumb-item"><span class="breadcrumb-text">Non Perbankan</span></div>
+              <div class="breadcrumb-item"><span class="breadcrumb-text breadcrumb-active">PNM</span></div>
             </div>
           </div>
         </div>
@@ -35,11 +35,22 @@
       <section class="section-md bg-transparent text-left">
         <div class="container">
           <div class="text-block text-block-1">
-            <img src="<?php echo base_url();?>theme/images/akses_permodalan/kur.png" alt="KUR" width="80px" height="auto" style="float:left;">
-            <h2>KUR</h2>
+            <img src="<?php echo base_url();?>theme/images/akses_permodalan/pnm.jpg" alt="Permodalan Nasional Madani (PNM)" width="50px" height="auto" style="float:left;">
+            <h2>PNM</h2>
             <br/>
             <p class="big" style="text-align: justify !important;">
-            &emsp;&emsp;<strong>Kredit Usaha Rakyat (KUR)</strong> adalah kredit/pembiayaan modal kerja dan/atau investasi kepada debitur individu/perseorangan, badan usaha dan/atau kelompok usaha yang produktif dan layak namun belum memiliki agunan tambahan atau agunan tambahan belum cukup. Penyaluran KUR dapat dilakukan langsung, maksudnya UMKM dan Koperasi dapat langsung mengakses KUR di Kantor Cabang atau Kantor Cabang Pembantu Bank Pelaksana. Untuk lebih mendekatkan pelayanan kepada usaha mikro, maka penyaluran KUR dapat juga dilakukan secara tidak langsung, maksudnya usaha mikro dapat mengakses KUR melalui Lembaga Keuangan Mikro dan KSP/USP Koperasi, atau melalui kegiatan linkage program lainnya yang bekerjasama dengan Bank Pelaksana.
+              <strong>Permodalan Nasional Madani (PNM)</strong>
+              <br/><strong>1. PNM ULaMM & PNM ULaMM Syariah</strong>
+              <br/>• <strong>PNM ULaMM</strong> menjadi gerai layanan di bawah satu atap atau “One Stop Shopping” bagi para pengusaha mikro dan kecil, yang dilengkapi dengan berbagai dukungan teknis bagi peminjam. Tujuannya adalah membantu usaha mikro dan kecil agar terus berkembang sekaligus mempercepat kemajuan usahanya. PNM ULaMM dilengkapi dengan pelatihan, jasa konsultasi, pendampingan, serta dukungan pengelolaan keuangan dan akses pasar bagi nasabah.
+              <br/>•  <strong>PNM ULAMM SYARIAH</strong> merupakan penyaluran pembiayaan yang dilakukan sesuai ketentuan prinsip syariah yang berdasarkan fatwa dan/atau pernyataan kesesuaian syariah dari Dewan Syariah Nasional Majelis Ulama Indonesia bagi pelaku usaha UMKM.
+
+              <br/><strong>2. Program Pengembangan Kapasitas Usaha</strong>
+              <br/>&emsp;&emsp;PNM selalu menghadirkan layanan yang optimal bagi nasabah, salah satunya dengan meluncurkan program Pengembangan Kapasitas Usaha dan Pengembangan Kelompok (PKU-PK) pada tahun 2010. Program PKU-PK bertujuan untuk memberikan pendampingan dan pembinaan kepada pelaku usaha ultra mikro, mikro dan kecil yang merupakan nasabah binaan PNM.
+
+              <br/><strong>3. Jasa Manajemen & PKBL</strong>
+              <br/>• <strong>Jasa Manajemen.</strong> Merupakan Perseroan memiliki dua aktivitas Jasa Manajemen, yaitu Jasa Manajemen untuk penguatan lembaga keuangan dan sektor riil. Selain itu Jasa Manajemen PNM memiliki program Kemitraan berupa penyaluran dana kemitraan yang ditujukan kepada pelaku UMKM.
+              <br/>• <strong>Program Kemitraan.</strong> Sebagai bentuk komitmen Perseroan dalam mendorong pemberdayaan UMKM di Indonesia, Perseroan memiliki Program Kemitraan berupa penyaluran dana kemitraan yang ditujukan kepada UMKM. 
+              <br/>• <strong>Program Kemitraan dan Bina Lingkungan (PKBL).</strong> PNM senantiasa menjalin hubungan yang harmonis dengan masyarakat dan lingkungan sekitar. Upaya ini dituangkan melalui program-program yang digagas melalui Program Kemitraan dan Bina Lingkungan (PKBL).
             </p>
           </div>
         </div>

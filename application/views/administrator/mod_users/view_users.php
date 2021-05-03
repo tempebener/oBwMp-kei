eb64f152533c4f49fcbba2e7443295bd594b5edd
tempebener <bayoewa@gmail.com>
2021-05-03 10:50:52 +0700

    Update view_users.php

--- application/views/administrator/mod_users/view_users.php
+++ application/views/administrator/mod_users/view_users.php
@@ -24,34 +24,34 @@
                     foreach ($record as $row){
                         
                     if ($row['foto'] == ''){ $foto ='admin.png'; }else{ $foto = $row['foto']; } ?>
-                            <tr><td><?= $no ?></td>
-                              <td><?= $row['username'] ?></td>
-                              <td><?= $row['nama_lengkap'] ?></td>
-                              <td><?= $row['email'] ?></td>
-                              <td class="div-image"><img class="img-circle" width="45px" style="border:1px solid #cecece" class="img-circle" src="<?= base_url('theme/images/foto_register/'.$foto) ?>"></td>
-                              <td>
-                                <?php
-                                  if($row['blokir']=="Y"){
-                                      echo "<span class='label label-warning fontspecial'>Blokir</span>";
-                                  }else{
-                                      echo "<span class='label label-info fontspecial'>Aktif</span>";
-                                  }
-                                ?>
-                              </td>
-                              <td><?= $row['level'] ?></td>
-                              <td><center>
-                                <a class="btn btn-success btn-xs" title="Edit Data" href="<?= base_url('admin/administrator/edit_manajemenuser/'.$row['id_users']) ?>"><span class="glyphicon glyphicon-edit"></span></a>
+                        <tr><td><?= $no ?></td>
+                          <td><?= $row['username'] ?></td>
+                          <td><?= $row['nama_lengkap'] ?></td>
+                          <td><?= $row['email'] ?></td>
+                          <td class="div-image"><img class="img-circle" width="45px" style="border:1px solid #cecece" class="img-circle" src="<?= base_url('theme/images/foto_register/'.$foto) ?>"></td>
+                          <td>
+                            <?php
+                              if($row['blokir']=="Y"){
+                                  echo "<span class='label label-warning fontspecial'>Blokir</span>";
+                              }else{
+                                  echo "<span class='label label-info fontspecial'>Aktif</span>";
+                              }
+                            ?>
+                          </td>
+                          <td><?= $row['level'] ?></td>
+                          <td><center>
+                            <a class="btn btn-success btn-xs" title="Edit Data" href="<?= base_url('admin/administrator/edit_manajemenuser/'.$row['id_users']) ?>"><span class="glyphicon glyphicon-edit"></span></a>
 
-                                <?php
-                                  if($row['blokir']=="Y"){
-                                  echo "<a class='btn btn-info btn-xs' title='Aktifkan User' href='".base_url()."admin/administrator/active_manajemenuser/$row[id_users]'><span class='glyphicon glyphicon-ok'></span></a>"; 
-                                  }else{
-                                  echo "<a class='btn btn-warning btn-xs' title='Blokir User' href='".base_url()."admin/administrator/deactive_manajemenuser/$row[id_users]' onclick=\"return confirm('Apa anda yakin untuk blokir User ini?')\"><span class='glyphicon glyphicon-ban-circle'></span></a>"; 
-                                  } ?>
+                            <?php
+                              if($row['blokir']=="Y"){
+                              echo "<a class='btn btn-info btn-xs' title='Aktifkan User' href='".base_url()."admin/administrator/active_manajemenuser/$row[id_users]'><span class='glyphicon glyphicon-ok'></span></a>"; 
+                              }else{
+                              echo "<a class='btn btn-warning btn-xs' title='Blokir User' href='".base_url()."admin/administrator/deactive_manajemenuser/$row[id_users]' onclick=\"return confirm('Apa anda yakin untuk blokir User ini?')\"><span class='glyphicon glyphicon-ban-circle'></span></a>"; 
+                              } ?>
 
-                                <a class="btn btn-danger btn-xs" title="Delete Data" href="<?= base_url('admin/administrator/delete_manajemenuser/'.$row['id_users']) ?>" onclick="return confirm('Apa anda yakin untuk hapus Data ini?')"><span class="glyphicon glyphicon-remove"></span></a>
-                              </center></td>
-                          </tr>
+                            <a class="btn btn-danger btn-xs" title="Delete Data" href="<?= base_url('admin/administrator/delete_manajemenuser/'.$row['id_users']) ?>" onclick="return confirm('Apa anda yakin untuk hapus Data ini?')"><span class="glyphicon glyphicon-remove"></span></a>
+                          </center></td>
+                      </tr>
                   <?php
                       $no++;
                     }

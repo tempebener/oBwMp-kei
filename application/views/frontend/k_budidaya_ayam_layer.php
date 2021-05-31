974a8792945b218da3f24bcbb450ebfae2785ecb
sansD92 <adjiedwisandy@gmail.com>
2021-05-31 11:27:28 +0700

    up

--- application/views/frontend/k_budidaya_ayam_layer.php
+++ application/views/frontend/k_budidaya_ayam_layer.php
@@ -40,13 +40,45 @@
           <div class="row row-30">
             <div class="col-md-6 col-xl-12 d-flex">
               <!-- <div class="image image-responsive rounded" style="background-image:url(<?php echo base_url();?>theme/images/profil/sapi.png)"></div> -->
-              <img class="image-custom rounded" src="<?php echo base_url();?>theme/images/skema_kemitraan/skema_kemitraan_budidaya_ayam_layer.png" alt="SKEMA KEMITRAAN Budidaya Ayam Layer" width="100%" height="auto">
+              <img class="image-custom rounded" src="<?php echo base_url();?>theme/images/skema_kemitraan/bal.png" alt="SKEMA KEMITRAAN Budidaya Ayam Layer" width="100%" height="auto">
             </div>
           
           </div>
         </div>
       </section>
-
+ <section class="section section-md bg-transparent">
+        <div class="container">
+          <dl class="term-list">
+            <dt class="h6">A. Skema Kemitraan Budidaya Ayam Layer</dt>
+            <dd>Skema kemitraan budidaya ayam layer merupakan suatu strategis bisnis dalam bentuk kerja sama antara kelompok peternak/UMKM/peternak perorangan/Koperasi Pesantren Indonesia, Yayasan Kesatriaan Entrepreneur Indonesia dan PT. Widodo Makmur Unggas Tbk untuk meraih manfaat bersama maupun keuntungan bersama sesuai prinsip saling membutuhkan dan saling mengisi sesuai kesepakatan yang muncul.
+            </dd>
+          </dl>
+          <dl class="term-list">
+            <dt class="h6">KET</dt>
+            <dd>1.  Pihak Yayasan Kesatriaan Entrepreneur Indonesia melakukan seleksi terhadap Mitra</dd>
+            <dd>2.  Pihak Yayasan Kesatriaan Entrepreneur Indonesia memfasilitasi akses ke perbankan sebagai lembaga pembiayaan</dd>
+            <dd>3.  MoU antara PT. Widodo Makmur Unggas Tbk sebagai penyedia bahan baku dan offtaker dengan lembaga pembiayaan</dd>
+            <dd>4.  Perjanjian kerja sama antara Mitra dan PT. Widodo Makmur Unggas Tbk</dd>
+            <dd>5.  Pengajuan kredit (KUR) berupa kredit investasi dan kredit modal kerja oleh Mitra ke lembaga pembiayaan</dd>
+            <dd>6.  Survei, persetujuan, akad dan pencairan </dd>
+            <dd>7.  Pembangunan fasilitas kandang menggunakan dana dari kredit investasi yang telah di cairkan </dd>
+            <dd>8.  Pihak pembiayaan dalam hal ini perbankan melakukan pembayaaran bahan baku ke PT. Widodo Makmur Unggas Tbk atas perintah pembayaran dari Mitra </dd>
+            <dd>9.  PT. Widodo Makmur Unggas Tbk melakukan suplai bahan baku kebutuhan peternak</dd>
+            <dd>10. PT. Widodo Makmur Unggas Tbk melakukan pendampingan teknis kepada Mitra</dd>
+            <dd>11. Pihak Mitra menjual hasil panen kepada Pihak PT. Widodo Makmur Unggas Tbk</dd>
+            <dd>12. PT. Widodo Makmur Unggas Tbk melakukan pembayaran hasil penjualan ke lembaga pembiayaan</dd>
+            <dd>13. Lembaga pembiayaan melakukan pembayaran keuntungan ke Mitra</dd>
+          </dl>
+          <dl class="term-list">
+            <dt class="h6">Catatan</dt>
+            <dd>• Yayasan Kesatrian Entreprenuer Indonesia (YKEI) : sebagai pihak yang memfasilitasi akses ke perbankan dan melakukan seleksi kepada Mitra</dd>
+            <dd>• Mitra : kelompok peternak/UMKM/peternak perorangan/Koperasi Pesantren Indonesia</dd>
+            <dd>• PT. Widodo Makmur Unggas Tbk : sebagai pihak penyedia bahan baku dan oftaker hasil panen</dd>
+            <dd>• Lembaga pembiayaan : lembaga yang menyediakan kredit investasi dan kredit modal kerja 
+          </dl>
+         
+        </div>
+      </section>
       <!-- Footer start-->
       <?php $this->load->view('frontend/footer')?>
       <!-- Footer end-->

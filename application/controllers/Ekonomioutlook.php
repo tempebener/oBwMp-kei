8d54cf60a4322da826735f7b893d7b97a4986cab
arkans <82564511+arkansys22@users.noreply.github.com>
2021-07-21 11:25:01 +0700

    up

--- application/controllers/Ekonomioutlook.php
+++ application/controllers/Ekonomioutlook.php
@@ -13,11 +13,11 @@ class Ekonomioutlook extends CI_Controller {
 
   }
 
-	
+
 
 public function ekonomi_singel($id){
 
-			
+
 			$query = $this->Model_utama->view_where('tbl_ekonomi_outlook',array('judul_eo_seo' => $this->uri->segment(3)),'id_eo','DESC',0,1);
 			if ($query->num_rows()<=0){
 				redirect('main');
@@ -26,11 +26,11 @@ public function ekonomi_singel($id){
 				$row = $query->row_array();
 				$data['title'] = cetak($row['judul_eo']);
 				$data['description'] = cetak_meta($row['deskripsi_eo'],0,400);
-				$data['rows'] = $row;		
+				$data['rows'] = $row;
 
 				 $data['eo_detail']            = $this->M_ekonomi->get_by_id_ekonomi($id);
- 
-				  
+ 			 	$data['event_terbaru'] = $this->db->query("select * from tbl_event_detail  ORDER BY  date_event_detail desc limit 1")->result();
+
 
 				$this->load->view('frontend/ekonomi_outlook_singel', $data);
 			}
@@ -38,8 +38,8 @@ public function ekonomi_singel($id){
 
 
 
-	
-	
-    
+
+
+
 
 }
